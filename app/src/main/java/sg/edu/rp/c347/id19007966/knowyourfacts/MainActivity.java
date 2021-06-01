49ffcbe37b72fb08a0268edbc96c75934b853a6b
fattahyousaini <50009909+fattahyousaini@users.noreply.github.com>
2021-06-01 13:23:01 +0800

    Part1 MainActivity

--- app/src/main/java/sg/edu/rp/c347/id19007966/knowyourfacts/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/knowyourfacts/MainActivity.java
@@ -2,17 +2,54 @@ package sg.edu.rp.c347.id19007966.knowyourfacts;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Fragment> al;
+    FragmentPagerAdapter adapter;
+    ViewPager viewPager;
+    Button btnReadLater;
+
+    int reqCode = 12345;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewpager1);
+        btnReadLater = findViewById(R.id.btnBack);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+        al.add(new Frag3());
+
+
+        adapter = new FactsFragmentPagerAdapter(fm, al);
+
+        viewPager.setAdapter(adapter);
+
+        btnReadLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+//                System.exit(0);
+            }
+        });
     }
 
     @Override
@@ -25,7 +62,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_previous) {
-
+            if(viewPager.getCurrentItem() > 0){
+                int previousPage = viewPager.getCurrentItem() - 1;
+                viewPager.setCurrentItem(previousPage, true);
+            }
         }
         else if (id == R.id.action_random) {
 
@@ -35,4 +75,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
