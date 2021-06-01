f0414c8e19c2acddf3674c36a0b4d24d403313dd
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-06-01 12:25:44 +0800

    menu implementation, awaits for further collab

--- app/src/main/java/sg/edu/rp/c347/id19007966/knowyourfacts/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/knowyourfacts/MainActivity.java
@@ -1,8 +1,11 @@
 package sg.edu.rp.c347.id19007966.knowyourfacts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_previous) {
+
+        }
+        else if (id == R.id.action_random) {
+
+        }
+        else if (id == R.id.action_next) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
